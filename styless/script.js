4b71b4a99f116faed43d583a04c59814d9939a39
Bhanu Chaitanya <143734650+chaitanyabhanu@users.noreply.github.com>
2024-08-02 12:37:12 +0530

    Initial commit

---
+++ styless/script.js
@@ -0,0 +1,21 @@
+
+
+// Example No :1
+
+let hero = 'Ram Charan';
+  console.log(hero); 
+
+
+//   Example No :2
+
+hero= 'Varun Taja';
+console.log(hero);
+
+//   Example No :3
+
+hero ='Allu Arjun';
+console.log(hero);
+
+
+
+
